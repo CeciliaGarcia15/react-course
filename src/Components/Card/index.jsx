d29726c7d020ed8ccb93147567a497d56ba38fb3
Cecilia Garcia <46695480+CeciliaGarcia15@users.noreply.github.com>
2024-10-20 16:51:05 -0300

    Ver productos en el side menu y guardar en el carrito los productos

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -6,14 +6,26 @@ import { PlusIcon } from "@heroicons/react/24/outline"
 const Card = (data) => {
     const context = useContext(ShoppingCartContext)
 
+    const showProduct=(productDetail)=>{
+        context.openProductDetail()
+        context.setProductToShow(productDetail)
+    }
+
+    const addProductsToCart = (productData) =>{
+        context.setCount(context.count + 1)
+        context.setCartProducts([...context.cartProducts, productData])
+    }
     return(
-        <div className="w-56 bg-white rounded-lg cursor-pointer h-60">
+        <div 
+        className="w-56 bg-white rounded-lg cursor-pointer h-60"
+        onClick={()=>showProduct(data.data)}
+        >
             <figure className="relative w-full mb-2 h-4/5">
                 <span className="absolute bottom-0 left-0 px-3 py-0.5 m-2 text-xs text-black rounded-lg bg-white/60">{data.data.category.name}</span>
                 <img className="object-cover w-full h-full rounded-lg" src={data.data.images[0]} alt={data.data.title} />
                 <div 
                 className="absolute top-0 right-0 flex items-center justify-center w-6 h-6 p-1 m-2 bg-white rounded-full"
-                onClick={()=> context.setCount(context.count + 1)}
+                onClick={()=> addProductsToCart(data.data)}
                 >
                     <PlusIcon className="size-6"/>
                 </div>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,11 +5,35 @@ export const ShoppingCartContext = createContext()
 
 
 export const ShoppingCartProvider = ({children}) => {
-   const [count,setCount] = useState(0);
-    return(
+   
+   //Shopping Cart - Increment Quantity
+    const [count,setCount] = useState(0);
+
+    //Product Detail - Show-Close
+   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+   const openProductDetail = ()=> setIsProductDetailOpen(true)
+   const closeProductDetail = ()=> setIsProductDetailOpen(false)
+   
+
+      //Product Detail - Show Product
+   const [productToShow, setProductToShow] = useState({});
+
+
+   //Shopping cart - add products to cart
+   const [cartProducts, setCartProducts] = useState([]);
+
+   
+   return(
         <ShoppingCartContext.Provider value={{
             count,
             setCount,
+            isProductDetailOpen,
+            openProductDetail,
+            closeProductDetail,
+            productToShow,
+            setProductToShow,
+            cartProducts,
+            setCartProducts,
         }}>
             {children}
         </ShoppingCartContext.Provider>
