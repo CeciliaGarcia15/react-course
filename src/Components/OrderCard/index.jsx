a6a87d829a3ed10353226980bf0d2baa32c4b5bc
CeciliaGarcia15 <cecilia15-5@hotmail.com>
2024-10-23 16:38:47 -0300

    elimina un producto de la order y tambien realiza el checkout

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,27 +1,28 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
-
-
-const OrderCard = props => {
-
-    const {title,imageUrl,price} = props
-    return(
-        <div className="flex items-center justify-between mb-3">
-            <div className="flex items-center gap-2">
-                <figure className="size-20">
-                    <img className="object-cover w-full h-full rounded-lg" 
-                    src={imageUrl} 
-                    alt={title} />
-                </figure>
-                <p className="text-sm font-light ">{title}</p>
-            </div>
-            <div className="flex items-center gap-2">
-                <p className="text-lg font-medium ">{price}</p>
-                <XMarkIcon className="text-black size-6" />
-
-            </div>
-        </div>
-    )
+const OrderCard = (props) => {
+  const { id, title, imageUrl, price, handleDelete } = props;
+  return (
+    <div className="flex items-center justify-between mb-3">
+      <div className="flex items-center gap-2">
+        <figure className="size-20">
+          <img
+            className="object-cover w-full h-full rounded-lg"
+            src={imageUrl}
+            alt={title}
+          />
+        </figure>
+        <p className="text-sm font-light ">{title}</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <p className="text-lg font-medium ">{price}</p>
+        <XMarkIcon
+          onClick={() => handleDelete(id)}
+          className="text-black size-6"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default OrderCard;
