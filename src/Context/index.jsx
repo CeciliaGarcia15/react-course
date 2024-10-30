605dce7dc6fc628a13259da71572e1054e6ffa38
Cecilia Garcia <46695480+CeciliaGarcia15@users.noreply.github.com>
2024-10-29 22:53:37 -0300

    filtrado por categoria

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -27,11 +27,15 @@ export const ShoppingCartProvider = ({ children }) => {
 
   //Get Products
   const [items, setItems] = useState(null);
-  
+  const [filteredItems, setFilteredItems] = useState(null);
+
   
   //Get products by title
   const [searchByTitle, setSearchByTitle] = useState(null);
-  console.log('busqueda:',searchByTitle)
+
+  //Get products by category
+  const [searchByCategory, setSearchByCategory] = useState(null);
+
 
 
   useEffect(() => {
@@ -40,6 +44,41 @@ export const ShoppingCartProvider = ({ children }) => {
       .then((data) => setItems(data));
   }, []);
   
+  const filteredItemsByTitle = (items,searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  const filteredItemsByCategory = (items,searchByCategory) => {
+    return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+  }
+
+  const filterBy = (searchType,items,searchByTitle,searchByCategory) => {
+    if(searchType === 'BY_TITLE'){
+      return filteredItemsByTitle(items,searchByTitle)
+    }
+    if(searchType === 'BY_CATEGORY'){
+      return filteredItemsByCategory(items,searchByCategory)
+    }
+
+    if(searchType === 'BY_TITLE_AND_CATEGORY'){
+      return filteredItemsByCategory(items,searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    if(!searchType){
+      return items
+    }
+
+  }
+
+  useEffect(() => {
+    if(searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY',items,searchByTitle,searchByCategory))
+    if(searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE',items,searchByTitle,searchByCategory))
+    if(!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY',items,searchByTitle,searchByCategory))
+    if(!searchByTitle && !searchByCategory) setFilteredItems(filterBy(NULL,items,searchByTitle,searchByCategory))
+
+    }, [items,searchByTitle,searchByCategory]);
+
+
 
   return (
     <ShoppingCartContext.Provider
@@ -62,6 +101,10 @@ export const ShoppingCartProvider = ({ children }) => {
         setItems,
         searchByTitle,
         setSearchByTitle,
+        filteredItems,
+        searchByCategory,
+        setSearchByCategory
+
       }}
     >
       {children}
